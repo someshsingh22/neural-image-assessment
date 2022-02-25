fa4afd3e528b7bd4f88fd3ce1afb0d5b89163647
Somesh Kumar Singh <f20180175@goa.bits-pilani.ac.in>
2022-02-25 06:57:58 +0530

    Update nasnet.py

--- utils/nasnet.py
+++ utils/nasnet.py
@@ -57,7 +57,7 @@ from keras.regularizers import l2
 from keras.utils.data_utils import get_file
 from keras.engine.topology import get_source_inputs
 from keras_applications.imagenet_utils import _obtain_input_shape
-from keras_applications.inception_v3 import preprocess_input
+from keras.applications.inception_v3 import preprocess_input
 from keras_applications.imagenet_utils import decode_predictions
 from keras import backend as K
 
